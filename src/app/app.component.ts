31f426df5d80af34f0bd24faa7df2a934aff1d26
Dave Mullally <mullallydavid7@gmail.com>
2023-08-12 22:21:29 +0200

    use ng<for directive to display posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,45 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'posts';
+  posts = [
+    {
+      imgURL: 'assets/images/tree.jpeg',
+      title: 'Neat Tree',
+      username: 'JohnThough',
+      content: 'Climbed this beautiful tree today',
+    },
+    {
+      imgURL: 'assets/images/mountain.jpeg',
+      title: 'Snowy Mountain',
+      username: 'JohnDough',
+      content: 'Climbed this beautiful mountain today',
+    },
+    {
+      imgURL: 'assets/images/biking.jpeg',
+      title: 'Biker',
+      username: 'JohnDoh',
+      content: 'Climbed onto this beautiful bike today',
+    },
+    {
+      imgURL: 'assets/images/tree.jpeg',
+      title: 'Neat Tree',
+      username: 'JohnThough',
+      content: 'Climbed this beautiful tree today',
+    },
+    {
+      imgURL: 'assets/images/mountain.jpeg',
+      title: 'Snowy Mountain',
+      username: 'JohnDough',
+      content: 'Climbed this beautiful mountain today',
+    },
+    {
+      imgURL: 'assets/images/biking.jpeg',
+      title: 'Biker',
+      username: 'JohnDoh',
+      content: 'Climbed onto this beautiful bike today',
+    },
+  ];
 }
